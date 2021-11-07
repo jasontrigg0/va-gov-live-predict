15c600045d93052c936ab12a973d674714b98452
Jason Trigg <jasontrigg0@gmail.com>
2021-11-07 13:26:25 -0500

    Basic pa scrape info, that\ll be a big project...

---
+++ scrape_pa.py
@@ -0,0 +1,42 @@
+#every county needs its own scraper
+#see here for clarity, el30 parsers, think these are two of the more common types
+#https://github.com/openelections/openelections-data-pa/tree/master/parsers
+
+#http://www.adamscounty.us/Dept/ElectVoteReg/Pages/ElectionResults.aspx
+#http://www.adamscounty.us/Dept/ElectVoteReg/Documents/Election2021/NovemberResults/precinctsummary.pdf
+
+#https://www.alleghenycounty.us/elections/election-results.aspx
+#https://results.enr.clarityelections.com/PA/Allegheny/111176/web.278093/
+
+#https://co.armstrong.pa.us/index.php/departments-m/elections-voters-registration-m/election-results-m-evr
+#https://co.armstrong.pa.us/images/resources/electionresults/unofficial/1.pdf --> https://co.armstrong.pa.us/images/resources/electionresults/unofficial/62.pdf
+
+#http://www.beavercountypa.gov/Depts/Elections/Pages/ElectionResults.aspx
+#http://www.beavercountypa.gov/Depts/Elections/Documents/Nov2021Results/PrecinctsResults11022021.pdf
+
+#https://www.bedfordcountypa.org/departments/elections/election_results.php
+#https://www.bedfordcountypa.org/UnofficialElectionSummaryReportRPT.pdf
+
+#https://www.co.berks.pa.us/Dept/Elections/Pages/ElectionResults(ElectionNightResults).aspx
+#no precinct level reporting?
+#http://elections.co.berks.pa.us/results/default.aspx
+
+#http://www.blairco.org/Dept/Elections/Pages/Election-Results.aspx
+#http://www.blairco.org/Dept/Elections/Election%20Results/2021%20General%20Election/Unofficial%20Precinct%20Summary.pdf
+
+#https://bradfordcountypa.org/department/elections/
+#https://bradfordcountypa.org/wp-content/uploads/2021/11/2021-General-Results.pdf
+
+#https://www.buckscounty.gov/1206/2021-Election-Results
+#https://www.livevoterturnout.com/BucksPA/LiveResults/en/Index_6.html (not sure exactly)
+
+#https://www.butlercountypa.gov/683/Election-Returns-2020---Present
+#https://www.butlercountypa.gov/DocumentCenter/View/3733/SUMMARY
+
+#http://elections.cambriacountypa.gov/elections.webclient/
+#http://elections.cambriacountypa.gov/Elections.Webclient/Default.aspx?PageLayout=BYPRECINCT&Election=48&Precinct=1484 (maybe json to use here?)
+
+#http://www.cameroncountypa.com/resident/elections/precinct_results_(unofficial).php#outer-157
+#http://www.cameroncountypa.com/UNOFFICIAL%20W-MAIL-IN%20AND%20ABS_Precinct%20Summary.pdf
+
+#
